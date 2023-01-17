9caacdfad2f9f51054e7d677f150d6c87f299470
Jennifer Reif <jennifer.reif@neo4j.com>
2023-01-17 16:52:12 -0600

    Update SpringBoot and Java versions

--- src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersionController.java
+++ src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersionController.java
@@ -12,5 +12,11 @@ public class JavaVersionController {
     private final JavaVersionRepo javaVersionRepo;
 
     @GetMapping
-    Iterable<JavaVersionProjection> findAllJavaVersions() { return javaVersionRepo.findAllJavaVersions(); }
+    Iterable<JavaVersion> findAllJavaVersions() { return javaVersionRepo.findAll(); }
+
+    @GetMapping("/custom")
+    Iterable<JavaVersion> findAllCustom() { return javaVersionRepo.findAllCustom(); }
+
+    @GetMapping("/projection")
+    Iterable<JavaVersionProjection> findAllProjections() { return javaVersionRepo.findAllProjections(); }
 }
--- src/main/java/com/jmhreif/pouringcoffeeintomatrix/VersionDiff.java
+++ src/main/java/com/jmhreif/pouringcoffeeintomatrix/VersionDiff.java
@@ -1,14 +1,12 @@
 package com.jmhreif.pouringcoffeeintomatrix;
 
 import lombok.Data;
-import lombok.RequiredArgsConstructor;
 import org.springframework.data.neo4j.core.schema.GeneratedValue;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 
 @Node
 @Data
-@RequiredArgsConstructor
 public class VersionDiff {
     @Id
     @GeneratedValue
