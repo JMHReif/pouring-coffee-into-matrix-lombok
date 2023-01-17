b699324596833c51ef7daffb6b4c979368b1f70a
Jennifer Reif <jennifer.reif@neo4j.com>
2023-01-17 14:54:58 -0600

    Updates for SpringBoot and Java version

--- src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersion.java
+++ src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersion.java
@@ -5,10 +5,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.neo4j.core.schema.Id;
 import org.springframework.data.neo4j.core.schema.Node;
 import org.springframework.data.neo4j.core.schema.Property;
-import org.springframework.data.neo4j.core.schema.Relationship;
 
 import java.time.LocalDate;
-import java.util.List;
 
 @Node
 @Data
@@ -19,10 +17,4 @@ public class JavaVersion {
     private final String javaVersion;
     private String name, codeName, status, apiSpec;
     private LocalDate gaDate, eolDate;
-
-    @Relationship("FROM_NEWER")
-    private List<VersionDiff> olderVersionDiffs;
-
-    @Relationship("FROM_OLDER")
-    private List<VersionDiff> newerVersionDiffs;
 }
--- src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersionRepo.java
+++ src/main/java/com/jmhreif/pouringcoffeeintomatrix/JavaVersionRepo.java
@@ -1,9 +1,6 @@
 package com.jmhreif.pouringcoffeeintomatrix;
 
 import org.springframework.data.neo4j.repository.Neo4jRepository;
-import org.springframework.data.neo4j.repository.query.Query;
 
 public interface JavaVersionRepo extends Neo4jRepository<JavaVersion, String> {
-    @Query("MATCH (j:JavaVersion)-[r:FROM_NEWER]->(d:VersionDiff) RETURN j, collect(r), collect(d);")
-    Iterable<JavaVersionProjection> findAllJavaVersions();
 }
